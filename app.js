7f388df0ffbd8a05c394826d3d9cf042b664f2bf
Juan Hoyos <juanhoyos1347@gmail.com>
2026-03-19 14:20:53 -0500

    add: api

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`Hola desde la instancia: ${os.hostname()}`);
+});
+
+// Endpoint que consume CPU para simular carga
+app.get('/heavy', (req, res) => {
+    let count = 0;
+    for (let i = 0; i < 1e9; i++) {
+        count += i;
+    }
+    res.send("Carga pesada procesada 🚀");
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.listen(3000, () => {
+    console.log("API corriendo en puerto 3000");
+});
